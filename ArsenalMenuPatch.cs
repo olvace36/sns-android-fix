73dcc469d55139ecd9fa5b9ddc22c48873f712c7
olvace36 <120215414+olvace36@users.noreply.github.com>
2026-06-10 07:30:16 +0700

    Update ArsenalMenuPatch.cs

--- ArsenalMenuPatch.cs
+++ ArsenalMenuPatch.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using HarmonyLib;
+using StardewModdingAPI;
 using StardewValley;
 using SwordAndSorcerySMAPI;
 
@@ -8,31 +9,32 @@ namespace SnsAndroidFix;
 [HarmonyPatch(typeof(ArsenalMenu), MethodType.Constructor)]
 public class ArsenalMenuPatch
 {
+    internal static IMonitor? Monitor;
+
     static void Postfix(ArsenalMenu __instance)
     {
-        var field = typeof(ArsenalMenu).GetField("invMenu", 
+        var field = typeof(ArsenalMenu).GetField("invMenu",
             BindingFlags.NonPublic | BindingFlags.Instance);
-        if (field == null) return;
+        if (field == null)
+        {
+            Monitor?.Log("invMenu field not found!", LogLevel.Error);
+            return;
+        }
 
         var invMenu = field.GetValue(__instance);
-        if (invMenu == null) return;
+        if (invMenu == null)
+        {
+            Monitor?.Log("invMenu is null!", LogLevel.Error);
+            return;
+        }
 
         var type = invMenu.GetType();
-
         var xField = type.GetField("xPositionOnScreen");
         var yField = type.GetField("yPositionOnScreen");
 
-        if (xField != null)
-        {
-            int x = (int)xField.GetValue(invMenu);
-            if (x < 0) xField.SetValue(invMenu, 0);
-        }
+        int x = (int)(xField?.GetValue(invMenu) ?? 0);
+        int y = (int)(yField?.GetValue(invMenu) ?? 0);
 
-        if (yField != null)
-        {
-            int y = (int)yField.GetValue(invMenu);
-            int maxY = Game1.uiViewport.Height - 280;
-            if (y > maxY) yField.SetValue(invMenu, maxY);
-        }
+        Monitor?.Log($"invMenu position: X={x}, Y={y}, ViewportH={Game1.uiViewport.Height}, ViewportW={Game1.uiViewport.Width}", LogLevel.Info);
     }
 }
