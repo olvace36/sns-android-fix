e0eb19fa293f151bc94332568f438fc4e8fb8f70
olvace36 <120215414+olvace36@users.noreply.github.com>
2026-06-10 15:41:03 +0700

    Update ArsenalMenuPatch.cs

--- ArsenalMenuPatch.cs
+++ ArsenalMenuPatch.cs
@@ -26,18 +26,45 @@ public class ArsenalMenuPatch
 
         var type = invMenu.GetType();
 
-        // fix squareSide=0
         int sq = (int)(type.GetField("squareSide")?.GetValue(invMenu) ?? 0);
-        if (sq == 0)
+        if (sq != 0) return;
+
+        int newSq = 64;
+        int hGap = 8;
+        int verticalGap = 8;
+        int xOff = newSq / 2;
+        int yOff = 0;
+        int rows = 3;
+        int capacity = 36;
+        int cols = capacity / rows;
+
+        int menuY = Game1.uiViewport.Height / 2 - 150 - 100 - IClickableMenu.borderWidth;
+        int menuH = 300 + IClickableMenu.borderWidth * 2;
+        int startX = (int)(type.GetField("xPositionOnScreen")?.GetValue(invMenu) ?? 0);
+        int startY = menuY + menuH + 8;
+
+        type.GetField("squareSide")?.SetValue(invMenu, newSq);
+        type.GetField("scaleFactor")?.SetValue(invMenu, (float)newSq / 64f);
+        type.GetField("yPositionOnScreen")?.SetValue(invMenu, startY);
+        type.GetField("xOffset")?.SetValue(invMenu, xOff);
+        type.GetField("yOffset")?.SetValue(invMenu, yOff);
+        type.GetField("hGap")?.SetValue(invMenu, hGap);
+
+        // rebuild slot bounds
+        var inventorySlots = type.GetField("inventory")?.GetValue(invMenu) as List<ClickableComponent>;
+        if (inventorySlots != null)
         {
-            int viewH = Game1.uiViewport.Height;
-            int viewW = Game1.uiViewport.Width;
-            int newSq = 64;
-            type.GetField("squareSide")?.SetValue(invMenu, newSq);
-            type.GetField("scaleFactor")?.SetValue(invMenu, (float)newSq / 64f);
-            Monitor?.Log($"fixed squareSide={newSq}", LogLevel.Info);
+            for (int j = 0; j < inventorySlots.Count; j++)
+            {
+                int row = j / cols;
+                int col = j % cols;
+                inventorySlots[j].bounds.X = startX + xOff + col * (newSq + hGap);
+                inventorySlots[j].bounds.Y = startY + yOff + row * (newSq + verticalGap);
+                inventorySlots[j].bounds.Width = newSq + hGap;
+                inventorySlots[j].bounds.Height = newSq + verticalGap;
+            }
         }
 
-        Monitor?.Log($"invMenu w={type.GetField("width")?.GetValue(invMenu)}, squareSide={type.GetField("squareSide")?.GetValue(invMenu)}", LogLevel.Info);
+        Monitor?.Log($"rebuilt slots: startX={startX}, startY={startY}, sq={newSq}, slots={inventorySlots?.Count}", LogLevel.Info);
     }
 }
