f71dada077ee68d9988d8514cd73fc9f730f4ed6
olvace36 <120215414+olvace36@users.noreply.github.com>
2026-06-10 16:16:55 +0700

    Update ArsenalMenuPatch.cs

--- ArsenalMenuPatch.cs
+++ ArsenalMenuPatch.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
 using System.Reflection;
 using HarmonyLib;
-using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewValley;
 using StardewValley.Menus;
@@ -21,50 +18,22 @@ public class ArsenalMenuPatch
             BindingFlags.NonPublic | BindingFlags.Instance);
         if (field == null) return;
 
-        var invMenu = field.GetValue(__instance);
-        if (invMenu == null) return;
+        var oldInvMenu = field.GetValue(__instance);
+        if (oldInvMenu == null) return;
 
-        var type = invMenu.GetType();
-
-        int sq = (int)(type.GetField("squareSide")?.GetValue(invMenu) ?? 0);
+        var type = oldInvMenu.GetType();
+        int sq = (int)(type.GetField("squareSide")?.GetValue(oldInvMenu) ?? 0);
         if (sq != 0) return;
-
-        int newSq = 64;
-        int hGap = 8;
-        int verticalGap = 8;
-        int xOff = newSq / 2;
-        int yOff = 0;
-        int rows = 3;
-        int capacity = 36;
-        int cols = capacity / rows;
 
         int menuY = Game1.uiViewport.Height / 2 - 150 - 100 - IClickableMenu.borderWidth;
         int menuH = 300 + IClickableMenu.borderWidth * 2;
-        int startX = (int)(type.GetField("xPositionOnScreen")?.GetValue(invMenu) ?? 0);
         int startY = menuY + menuH + 8;
+        int startX = Game1.xEdge;
 
-        type.GetField("squareSide")?.SetValue(invMenu, newSq);
-        type.GetField("scaleFactor")?.SetValue(invMenu, (float)newSq / 64f);
-        type.GetField("yPositionOnScreen")?.SetValue(invMenu, startY);
-        type.GetField("xOffset")?.SetValue(invMenu, xOff);
-        type.GetField("yOffset")?.SetValue(invMenu, yOff);
-        type.GetField("hGap")?.SetValue(invMenu, hGap);
+        // สร้าง InventoryMenu ใหม่
+        var newInvMenu = new InventoryMenu(startX, startY, true);
+        field.SetValue(__instance, newInvMenu);
 
-        // rebuild slot bounds
-        var inventorySlots = type.GetField("inventory")?.GetValue(invMenu) as List<ClickableComponent>;
-        if (inventorySlots != null)
-        {
-            for (int j = 0; j < inventorySlots.Count; j++)
-            {
-                int row = j / cols;
-                int col = j % cols;
-                inventorySlots[j].bounds.X = startX + xOff + col * (newSq + hGap);
-                inventorySlots[j].bounds.Y = startY + yOff + row * (newSq + verticalGap);
-                inventorySlots[j].bounds.Width = newSq + hGap;
-                inventorySlots[j].bounds.Height = newSq + verticalGap;
-            }
-        }
-
-        Monitor?.Log($"rebuilt slots: startX={startX}, startY={startY}, sq={newSq}, slots={inventorySlots?.Count}", LogLevel.Info);
+        Monitor?.Log($"replaced invMenu: X={startX}, Y={startY}", LogLevel.Info);
     }
 }
