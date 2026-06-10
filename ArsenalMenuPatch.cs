4f0ee3aad85c3eaf1306ea17680aed38aea846b4
olvace36 <120215414+olvace36@users.noreply.github.com>
2026-06-10 08:12:01 +0700

    Update ArsenalMenuPatch.cs

--- ArsenalMenuPatch.cs
+++ ArsenalMenuPatch.cs
@@ -12,29 +12,32 @@ public class ArsenalMenuPatch
     internal static IMonitor? Monitor;
 
     static void Postfix(ArsenalMenu __instance)
+{
+    // เพิ่มตรงนี้
+    Monitor?.Log($"ArsenalMenu: X={__instance.xPositionOnScreen}, Y={__instance.yPositionOnScreen}, W={__instance.width}, H={__instance.height}", LogLevel.Info);
+
+    var field = typeof(ArsenalMenu).GetField("invMenu",
+        BindingFlags.NonPublic | BindingFlags.Instance);
+    if (field == null)
     {
-        var field = typeof(ArsenalMenu).GetField("invMenu",
-            BindingFlags.NonPublic | BindingFlags.Instance);
-        if (field == null)
-        {
-            Monitor?.Log("invMenu field not found!", LogLevel.Error);
-            return;
-        }
+        Monitor?.Log("invMenu field not found!", LogLevel.Error);
+        return;
+    }
 
-        var invMenu = field.GetValue(__instance);
-        if (invMenu == null)
-        {
-            Monitor?.Log("invMenu is null!", LogLevel.Error);
-            return;
-        }
+    var invMenu = field.GetValue(__instance);
+    if (invMenu == null)
+    {
+        Monitor?.Log("invMenu is null!", LogLevel.Error);
+        return;
+    }
 
-        var type = invMenu.GetType();
-        var xField = type.GetField("xPositionOnScreen");
-        var yField = type.GetField("yPositionOnScreen");
+    var type = invMenu.GetType();
+    var xField = type.GetField("xPositionOnScreen");
+    var yField = type.GetField("yPositionOnScreen");
 
-        int x = (int)(xField?.GetValue(invMenu) ?? 0);
-        int y = (int)(yField?.GetValue(invMenu) ?? 0);
+    int x = (int)(xField?.GetValue(invMenu) ?? 0);
+    int y = (int)(yField?.GetValue(invMenu) ?? 0);
 
-        Monitor?.Log($"invMenu position: X={x}, Y={y}, ViewportH={Game1.uiViewport.Height}, ViewportW={Game1.uiViewport.Width}", LogLevel.Info);
+    Monitor?.Log($"invMenu position: X={x}, Y={y}, ViewportH={Game1.uiViewport.Height}, ViewportW={Game1.uiViewport.Width}", LogLevel.Info);
     }
 }
