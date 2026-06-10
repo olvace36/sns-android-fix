65abee26cd9d90e7113db66e443d6e047186e82f
olvace36 <120215414+olvace36@users.noreply.github.com>
2026-06-10 12:43:31 +0700

    Update ArsenalMenuPatch.cs

--- ArsenalMenuPatch.cs
+++ ArsenalMenuPatch.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Reflection;
 using HarmonyLib;
+using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewValley;
 using StardewValley.Menus;
@@ -26,7 +27,6 @@ public class ArsenalMenuPatch
 
         int oldY = (int)(type.GetField("yPositionOnScreen")?.GetValue(invMenu) ?? 0);
 
-        // คำนวณจาก menu จริง
         int menuY = Game1.uiViewport.Height / 2 - 150 - 100 - IClickableMenu.borderWidth;
         int menuH = 300 + IClickableMenu.borderWidth * 2;
         int newY = menuY + menuH + 8;
@@ -34,11 +34,22 @@ public class ArsenalMenuPatch
 
         type.GetField("yPositionOnScreen")?.SetValue(invMenu, newY);
 
+        // ขยับ slot bounds
         var inventorySlots = type.GetField("inventory")?.GetValue(invMenu) as List<ClickableComponent>;
         if (inventorySlots != null)
         {
             foreach (var slot in inventorySlots)
                 slot.bounds.Y += diff;
+        }
+
+        // update fadeRect
+        var fadeRectField = type.GetField("fadeRect", BindingFlags.NonPublic | BindingFlags.Instance);
+        if (fadeRectField != null)
+        {
+            var newFadeRect = new Rectangle(
+                0, newY, Game1.uiViewport.Width, Game1.uiViewport.Height - newY + 1);
+            fadeRectField.SetValue(invMenu, newFadeRect);
+            Monitor?.Log($"fadeRect updated to Y={newY}", LogLevel.Info);
         }
 
         Monitor?.Log($"menuY={menuY}, menuH={menuH}, oldY={oldY}, newY={newY}, diff={diff}, slots={inventorySlots?.Count}", LogLevel.Info);
