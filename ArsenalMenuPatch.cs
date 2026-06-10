57fba70b50abc0f17aa0391c0fc98ac4630499b4
olvace36 <120215414+olvace36@users.noreply.github.com>
2026-06-10 08:28:32 +0700

    Update ArsenalMenuPatch.cs

--- ArsenalMenuPatch.cs
+++ ArsenalMenuPatch.cs
@@ -13,30 +13,22 @@ public class ArsenalMenuPatch
 
     static void Postfix(ArsenalMenu __instance)
     {
-        var instanceType = typeof(ArsenalMenu);
-        int ax = (int)(instanceType.GetField("xPositionOnScreen", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy)?.GetValue(__instance) ?? -1);
-        int ay = (int)(instanceType.GetField("yPositionOnScreen", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy)?.GetValue(__instance) ?? -1);
-        Monitor?.Log($"ArsenalMenu: X={ax}, Y={ay}", LogLevel.Info);
-
-        var field = instanceType.GetField("invMenu",
+        var field = typeof(ArsenalMenu).GetField("invMenu",
             BindingFlags.NonPublic | BindingFlags.Instance);
-        if (field == null)
-        {
-            Monitor?.Log("invMenu field not found!", LogLevel.Error);
-            return;
-        }
+        if (field == null) return;
 
         var invMenu = field.GetValue(__instance);
-        if (invMenu == null)
-        {
-            Monitor?.Log("invMenu is null!", LogLevel.Error);
-            return;
-        }
+        if (invMenu == null) return;
 
         var type = invMenu.GetType();
         int x = (int)(type.GetField("xPositionOnScreen")?.GetValue(invMenu) ?? 0);
         int y = (int)(type.GetField("yPositionOnScreen")?.GetValue(invMenu) ?? 0);
 
-        Monitor?.Log($"invMenu: X={x}, Y={y}, ViewportH={Game1.uiViewport.Height}, ViewportW={Game1.uiViewport.Width}", LogLevel.Info);
+        Monitor?.Log($"invMenu before: X={x}, Y={y}", LogLevel.Info);
+
+        int newY = (Game1.uiViewport.Height - 280) / 2 + 100;
+        type.GetField("yPositionOnScreen")?.SetValue(invMenu, newY);
+
+        Monitor?.Log($"invMenu after: X={x}, Y={newY}", LogLevel.Info);
     }
 }
