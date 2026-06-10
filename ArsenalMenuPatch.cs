b2513c0c7e798c84e20ee812ea3c93ffa92c7b56
olvace36 <120215414+olvace36@users.noreply.github.com>
2026-06-10 14:23:15 +0700

    Update ArsenalMenuPatch.cs

--- ArsenalMenuPatch.cs
+++ ArsenalMenuPatch.cs
@@ -25,38 +25,18 @@ public class ArsenalMenuPatch
 
         var type = invMenu.GetType();
 
-        int oldY = (int)(type.GetField("yPositionOnScreen")?.GetValue(invMenu) ?? 0);
-        int w = (int)(type.GetField("width")?.GetValue(invMenu) ?? -1);
-        int h = (int)(type.GetField("height")?.GetValue(invMenu) ?? -1);
-        int xOff = (int)(type.GetField("xOffset")?.GetValue(invMenu) ?? -1);
-        int yOff = (int)(type.GetField("yOffset")?.GetValue(invMenu) ?? -1);
-        int sq = (int)(type.GetField("squareSide")?.GetValue(invMenu) ?? -1);
-        Monitor?.Log($"invMenu w={w}, h={h}, xOffset={xOff}, yOffset={yOff}, squareSide={sq}", LogLevel.Info);
-
-        int menuY = Game1.uiViewport.Height / 2 - 150 - 100 - IClickableMenu.borderWidth;
-        int menuH = 300 + IClickableMenu.borderWidth * 2;
-        int newY = menuY + menuH + 8;
-        int diff = newY - oldY;
-
-        type.GetField("yPositionOnScreen")?.SetValue(invMenu, newY);
-
-        var inventorySlots = type.GetField("inventory")?.GetValue(invMenu) as List<ClickableComponent>;
-        if (inventorySlots != null)
+        // fix squareSide=0
+        int sq = (int)(type.GetField("squareSide")?.GetValue(invMenu) ?? 0);
+        if (sq == 0)
         {
-            foreach (var slot in inventorySlots)
-                slot.bounds.Y += diff;
+            int viewH = Game1.uiViewport.Height;
+            int viewW = Game1.uiViewport.Width;
+            int newSq = Math.Min((int)(90f * ((float)viewH / 360f)), (int)(90f * ((float)viewW / 1280f)));
+            type.GetField("squareSide")?.SetValue(invMenu, newSq);
+            type.GetField("scaleFactor")?.SetValue(invMenu, (float)newSq / 64f);
+            Monitor?.Log($"fixed squareSide={newSq}", LogLevel.Info);
         }
 
-        var fadeRectField = type.GetField("fadeRect", BindingFlags.NonPublic | BindingFlags.Instance);
-        if (fadeRectField != null)
-        {
-            var newFadeRect = new Rectangle(0, 0, Game1.uiViewport.Width, Game1.uiViewport.Height);
-            fadeRectField.SetValue(invMenu, newFadeRect);
-        }
-
-        if (inventorySlots != null && inventorySlots.Count > 0)
-            Monitor?.Log($"slot[0] bounds: X={inventorySlots[0].bounds.X}, Y={inventorySlots[0].bounds.Y}", LogLevel.Info);
-
-        Monitor?.Log($"menuY={menuY}, menuH={menuH}, oldY={oldY}, newY={newY}, diff={diff}, slots={inventorySlots?.Count}", LogLevel.Info);
+        Monitor?.Log($"invMenu w={type.GetField("width")?.GetValue(invMenu)}, squareSide={type.GetField("squareSide")?.GetValue(invMenu)}", LogLevel.Info);
     }
 }
