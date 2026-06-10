be95fcd14f4ef723037eac1c3998c6115fe4aaa1
olvace36 <120215414+olvace36@users.noreply.github.com>
2026-06-10 13:55:44 +0700

    Update ArsenalMenuPatch.cs

--- ArsenalMenuPatch.cs
+++ ArsenalMenuPatch.cs
@@ -26,6 +26,12 @@ public class ArsenalMenuPatch
         var type = invMenu.GetType();
 
         int oldY = (int)(type.GetField("yPositionOnScreen")?.GetValue(invMenu) ?? 0);
+        int w = (int)(type.GetField("width")?.GetValue(invMenu) ?? -1);
+        int h = (int)(type.GetField("height")?.GetValue(invMenu) ?? -1);
+        int xOff = (int)(type.GetField("xOffset")?.GetValue(invMenu) ?? -1);
+        int yOff = (int)(type.GetField("yOffset")?.GetValue(invMenu) ?? -1);
+        int sq = (int)(type.GetField("squareSide")?.GetValue(invMenu) ?? -1);
+        Monitor?.Log($"invMenu w={w}, h={h}, xOffset={xOff}, yOffset={yOff}, squareSide={sq}", LogLevel.Info);
 
         int menuY = Game1.uiViewport.Height / 2 - 150 - 100 - IClickableMenu.borderWidth;
         int menuH = 300 + IClickableMenu.borderWidth * 2;
@@ -41,13 +47,11 @@ public class ArsenalMenuPatch
                 slot.bounds.Y += diff;
         }
 
-        // reset fadeRect ให้ครอบคลุมทั้งจอ
         var fadeRectField = type.GetField("fadeRect", BindingFlags.NonPublic | BindingFlags.Instance);
         if (fadeRectField != null)
         {
             var newFadeRect = new Rectangle(0, 0, Game1.uiViewport.Width, Game1.uiViewport.Height);
             fadeRectField.SetValue(invMenu, newFadeRect);
-            Monitor?.Log("fadeRect set to full screen", LogLevel.Info);
         }
 
         if (inventorySlots != null && inventorySlots.Count > 0)
