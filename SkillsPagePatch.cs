3cd4ba6bacb881f55a8e8b48a206e61bac37d938
olvace36 <120215414+olvace36@users.noreply.github.com>
2026-06-11 20:57:11 +0700

    Update SkillsPagePatch.cs

--- SkillsPagePatch.cs
+++ SkillsPagePatch.cs
@@ -28,8 +28,8 @@ public class SkillsPagePatch
     public static void Apply(IModHelper helper, IMonitor monitor, Harmony harmony)
     {
         Monitor = monitor;
-
         _newSkillsPageType = AccessTools.TypeByName("SpaceCore.Interface.NewSkillsPage");
+
         if (_newSkillsPageType != null)
         {
             var drawMethod = _newSkillsPageType.GetMethod("draw", new[] { typeof(SpriteBatch) });
@@ -44,9 +44,7 @@ public class SkillsPagePatch
         helper.Events.Display.MenuChanged += (s, e) =>
         {
             if (e.NewMenu is not GameMenu gameMenu) return;
-
-            var newSkillsPageType = AccessTools.TypeByName("SpaceCore.Interface.NewSkillsPage");
-            if (newSkillsPageType == null) return;
+            if (_newSkillsPageType == null) return;
 
             var pages = typeof(GameMenu).GetField("pages",
                 BindingFlags.Public | BindingFlags.Instance)
@@ -56,7 +54,7 @@ public class SkillsPagePatch
             int skillsTab = GameMenu.skillsTab;
             if (skillsTab >= pages.Count) return;
 
-            var constructor = newSkillsPageType.GetConstructor(new[]
+            var constructor = _newSkillsPageType.GetConstructor(new[]
             {
                 typeof(int), typeof(int), typeof(int), typeof(int)
             });
@@ -72,15 +70,15 @@ public class SkillsPagePatch
 
             int rightEdge = x + w - 64;
 
-            var upBtn = newSkillsPageType.GetField("upButton", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(newPage);
-            var downBtn = newSkillsPageType.GetField("downButton", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(newPage);
-            var scrollBtn = newSkillsPageType.GetField("scrollBar", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(newPage);
+            var upBtn = _newSkillsPageType.GetField("upButton", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(newPage);
+            var downBtn = _newSkillsPageType.GetField("downButton", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(newPage);
+            var scrollBtn = _newSkillsPageType.GetField("scrollBar", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(newPage);
 
             MoveButton(upBtn, rightEdge);
             MoveButton(downBtn, rightEdge);
             MoveButton(scrollBtn, rightEdge + 12);
 
-            var scrollBarRunnerField = newSkillsPageType.GetField("scrollBarRunner", BindingFlags.NonPublic | BindingFlags.Instance);
+            var scrollBarRunnerField = _newSkillsPageType.GetField("scrollBarRunner", BindingFlags.NonPublic | BindingFlags.Instance);
             if (scrollBarRunnerField != null)
             {
                 var runner = (Rectangle)scrollBarRunnerField.GetValue(newPage);
@@ -88,66 +86,71 @@ public class SkillsPagePatch
                 scrollBarRunnerField.SetValue(newPage, runner);
             }
 
-            // log ทุกอย่าง
-            var squareSide = newSkillsPageType.GetField("squareSide", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(newPage);
-            var scaleFactor = newSkillsPageType.GetField("scaleFactor", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(newPage);
-            var skillScrollOffset = newSkillsPageType.GetField("skillScrollOffset", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(newPage);
-            var maxSkillCountOnScreen = newSkillsPageType.GetProperty("MaxSkillCountOnScreen", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(newPage);
-            var allSkillCount = newSkillsPageType.GetProperty("AllSkillCount", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(newPage);
-            var skillBars = newSkillsPageType.GetField("skillBars", BindingFlags.Public | BindingFlags.Instance)?.GetValue(newPage) as System.Collections.IList;
-            var skillAreas = newSkillsPageType.GetField("skillAreas", BindingFlags.Public | BindingFlags.Instance)?.GetValue(newPage) as System.Collections.IList;
-            var visibleSkills = newSkillsPageType.GetProperty("VisibleSkills", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(newPage) as string[];
+            var skillScrollOffset = _newSkillsPageType.GetField("skillScrollOffset", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(newPage);
+            var maxSkillCountOnScreen = _newSkillsPageType.GetProperty("MaxSkillCountOnScreen", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(newPage);
+            var allSkillCount = _newSkillsPageType.GetProperty("AllSkillCount", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(newPage);
+            var skillBars = _newSkillsPageType.GetField("skillBars", BindingFlags.Public | BindingFlags.Instance)?.GetValue(newPage) as System.Collections.IList;
+            var skillAreas = _newSkillsPageType.GetField("skillAreas", BindingFlags.Public | BindingFlags.Instance)?.GetValue(newPage) as System.Collections.IList;
+            var visibleSkills = _newSkillsPageType.GetProperty("VisibleSkills", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(newPage) as string[];
             var upBounds = upBtn?.GetType().GetField("bounds")?.GetValue(upBtn);
             var downBounds = downBtn?.GetType().GetField("bounds")?.GetValue(downBtn);
 
             Monitor?.Log($"pos: x={x}, y={y}, w={w}, h={h}, rightEdge={rightEdge}", LogLevel.Info);
-            Monitor?.Log($"squareSide={squareSide}, scaleFactor={scaleFactor}", LogLevel.Info);
-            Monitor?.Log($"skillScrollOffset={skillScrollOffset}, maxOnScreen={maxSkillCountOnScreen}", LogLevel.Info);
-            Monitor?.Log($"allSkillCount={allSkillCount}, skillBars={skillBars?.Count}, skillAreas={skillAreas?.Count}", LogLevel.Info);
-            Monitor?.Log($"upButton bounds: {upBounds}, downButton bounds: {downBounds}", LogLevel.Info);
+            Monitor?.Log($"skillScrollOffset={skillScrollOffset}, maxOnScreen={maxSkillCountOnScreen}, allSkillCount={allSkillCount}", LogLevel.Info);
+            Monitor?.Log($"skillBars={skillBars?.Count}, skillAreas={skillAreas?.Count}", LogLevel.Info);
+            Monitor?.Log($"upButton: {upBounds}, downButton: {downBounds}", LogLevel.Info);
             Monitor?.Log($"scrollBarRunner: {scrollBarRunnerField?.GetValue(newPage)}", LogLevel.Info);
             Monitor?.Log($"VisibleSkills={visibleSkills?.Length}", LogLevel.Info);
             if (visibleSkills != null)
                 foreach (var skill in visibleSkills)
                     Monitor?.Log($"  skill: {skill}", LogLevel.Info);
-            foreach (var f in newSkillsPageType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
-                Monitor?.Log($"field: {f.Name} = {f.GetValue(newPage)}", LogLevel.Info);
 
             pages[skillsTab] = newPage;
             Monitor?.Log("SkillsPage replaced!", LogLevel.Info);
+        };
+
+        helper.Events.Display.RenderedActiveMenu += (s, e) =>
+        {
+            if (Game1.activeClickableMenu is not GameMenu gameMenu) return;
+            if (_newSkillsPageType == null) return;
+
+            var pages = typeof(GameMenu).GetField("pages", BindingFlags.Public | BindingFlags.Instance)
+                ?.GetValue(gameMenu) as System.Collections.Generic.List<IClickableMenu>;
+            if (pages == null || gameMenu.currentTab != GameMenu.skillsTab) return;
+
+            var page = pages[GameMenu.skillsTab];
+            if (page?.GetType() != _newSkillsPageType) return;
+
+            var showsAll = _newSkillsPageType.GetProperty("ShowsAllSkillsAtOnce",
+                BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(page);
+            Monitor?.Log($"RenderedActiveMenu: ShowsAllSkillsAtOnce={showsAll}", LogLevel.Info);
+            if (showsAll is true) return;
+
+            var upBtn = _newSkillsPageType.GetField("upButton", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(page);
+            var downBtn = _newSkillsPageType.GetField("downButton", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(page);
+            var scrollBarRunner = _newSkillsPageType.GetField("scrollBarRunner",
+                BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(page);
+            var scrollBtn = _newSkillsPageType.GetField("scrollBar", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(page);
+
+            var drawMethod = upBtn?.GetType().GetMethod("draw", new[] { typeof(SpriteBatch) });
+            Monitor?.Log($"drawMethod found: {drawMethod != null}, upBtn: {upBtn != null}, downBtn: {downBtn != null}", LogLevel.Info);
+
+            drawMethod?.Invoke(upBtn, new object[] { e.SpriteBatch });
+            drawMethod?.Invoke(downBtn, new object[] { e.SpriteBatch });
+
+            if (scrollBarRunner is Rectangle runner2)
+            {
+                IClickableMenu.drawTextureBox(e.SpriteBatch, Game1.mouseCursors,
+                    new Rectangle(403, 383, 6, 6),
+                    runner2.X, runner2.Y, runner2.Width, runner2.Height,
+                    Color.White, 4f, true, -1f);
+            }
+            drawMethod?.Invoke(scrollBtn, new object[] { e.SpriteBatch });
         };
     }
 
     public static void DrawPostfix(object __instance, SpriteBatch b)
     {
-        if (_newSkillsPageType == null) return;
-
-        var showsAll = _newSkillsPageType.GetProperty("ShowsAllSkillsAtOnce",
-            BindingFlags.NonPublic | BindingFlags.Instance)
-            ?.GetValue(__instance);
-        if (showsAll is true) return;
-
-        var upBtn = _newSkillsPageType.GetField("upButton",
-            BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(__instance);
-        var downBtn = _newSkillsPageType.GetField("downButton",
-            BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(__instance);
-        var scrollBarRunner = (Rectangle?)_newSkillsPageType.GetField("scrollBarRunner",
-            BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(__instance);
-        var scrollBtn = _newSkillsPageType.GetField("scrollBar",
-            BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(__instance);
-
-        var drawMethod = upBtn?.GetType().GetMethod("draw", new[] { typeof(SpriteBatch) });
-        drawMethod?.Invoke(upBtn, new object[] { b });
-        drawMethod?.Invoke(downBtn, new object[] { b });
-
-        if (scrollBarRunner.HasValue)
-        {
-            IClickableMenu.drawTextureBox(b, Game1.mouseCursors,
-                new Rectangle(403, 383, 6, 6),
-                scrollBarRunner.Value.X, scrollBarRunner.Value.Y,
-                scrollBarRunner.Value.Width, scrollBarRunner.Value.Height,
-                Color.White, 4f, true, -1f);
-        }
-        drawMethod?.Invoke(scrollBtn, new object[] { b });
+        Monitor?.Log("DrawPostfix called!", LogLevel.Info);
     }
 }
