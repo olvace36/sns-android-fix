dc729de6415786691a137c7fb441c56033b86f98
olvace36 <120215414+olvace36@users.noreply.github.com>
2026-06-11 10:40:55 +0700

    Update Entry method to modify health patch application

--- ModEntry.cs
+++ ModEntry.cs
@@ -8,8 +8,9 @@ public class ModEntry : Mod
     public override void Entry(IModHelper helper)
     {
         ArsenalMenuPatch.Monitor = Monitor;
+        RevalidateHealthPatch.Monitor = Monitor;
         var harmony = new Harmony(ModManifest.UniqueID);
-        LevelUpMenuTranspilerFix.Apply(harmony, Monitor);
+        LevelUpMenuTranspilerFix.Apply(harmony);
         harmony.PatchAll();
         GuidebookMenuPatch.Apply(harmony);
     }
