b98b3704b67c105a390cc2e7848c3258bfa3b348
olvace36 <120215414+olvace36@users.noreply.github.com>
2026-06-11 08:12:52 +0700

    Create LevelUpMenuTranspilerFix.cs

---
+++ LevelUpMenuTranspilerFix.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using HarmonyLib.Tools;
+
+namespace SnsAndroidFix;
+
+public class LevelUpMenuTranspilerFix
+{
+    public static void Apply(Harmony harmony)
+    {
+        var types = new[]
+        {
+            "SwordAndSorcerySMAPI.Framework.ModSkills.LevelUpMenuRevalidateHealthPatch",
+            "SwordAndSorcerySMAPI.Framework.ModSkills.LevelUpMenuRevalidateHealthPatchAgain"
+        };
+
+        foreach (var typeName in types)
+        {
+            var type = AccessTools.TypeByName(typeName);
+            if (type == null) continue;
+
+            var transpiler = type.GetMethod("Transpiler",
+                BindingFlags.Public | BindingFlags.Static);
+            if (transpiler == null) continue;
+
+            harmony.Patch(transpiler,
+                prefix: new HarmonyMethod(typeof(LevelUpMenuTranspilerFix)
+                    .GetMethod(nameof(SafeTranspilerPrefix))));
+        }
+    }
+
+    public static bool SafeTranspilerPrefix(MethodBase original,
+        ref IEnumerable<CodeInstruction> __result,
+        IEnumerable<CodeInstruction> insns)
+    {
+        try
+        {
+            return true;
+        }
+        catch
+        {
+            __result = insns;
+            return false;
+        }
+    }
+}
