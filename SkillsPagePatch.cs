87ba44b0886435b165c4224f4c70d517d7c2534f
olvace36 <120215414+olvace36@users.noreply.github.com>
2026-06-11 14:26:53 +0700

    Create SkillsPagePatch.cs

---
+++ SkillsPagePatch.cs
@@ -0,0 +1,45 @@
+using System.Reflection;
+using HarmonyLib;
+using StardewValley.Menus;
+
+namespace SnsAndroidFix;
+
+public class SkillsPagePatch
+{
+    public static void Apply(IModHelper helper)
+    {
+        helper.Events.Display.MenuChanged += (s, e) =>
+        {
+            if (e.NewMenu is not GameMenu gameMenu) return;
+
+            var newSkillsPageType = AccessTools.TypeByName("SpaceCore.Interface.NewSkillsPage");
+            if (newSkillsPageType == null) return;
+
+            var pages = typeof(GameMenu).GetField("pages",
+                BindingFlags.Public | BindingFlags.Instance)
+                ?.GetValue(gameMenu) as System.Collections.Generic.List<IClickableMenu>;
+            if (pages == null) return;
+
+            int skillsTab = GameMenu.skillsTab;
+            if (skillsTab >= pages.Count) return;
+
+            if (pages[skillsTab]?.GetType() == newSkillsPageType) return;
+
+            var constructor = newSkillsPageType.GetConstructor(new[]
+            {
+                typeof(int), typeof(int), typeof(int), typeof(int)
+            });
+            if (constructor == null) return;
+
+            var newPage = (IClickableMenu)constructor.Invoke(new object[]
+            {
+                gameMenu.xPositionOnScreen,
+                gameMenu.yPositionOnScreen,
+                gameMenu.width,
+                gameMenu.height
+            });
+
+            pages[skillsTab] = newPage;
+        };
+    }
+}
