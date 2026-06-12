235b3dac4fa98714a9a7ca01d9e1b0c0fe7d591d
olvace36 <120215414+olvace36@users.noreply.github.com>
2026-06-13 00:51:56 +0700

    Update ModEntry.cs

--- ModEntry.cs
+++ ModEntry.cs
@@ -25,31 +25,25 @@ public class ModEntry : Mod
 
         helper.Events.GameLoop.SaveLoaded += (s, e) =>
         {
-            var rogueSkill = AccessTools.TypeByName("SwordAndSorcerySMAPI.ModSnS")
-                ?.GetProperty("RogueSkill", BindingFlags.Public | BindingFlags.Static)
-                ?.GetValue(null);
-            var paladinSkill = AccessTools.TypeByName("SwordAndSorcerySMAPI.ModTOP")
-                ?.GetProperty("PaladinSkill", BindingFlags.Public | BindingFlags.Static)
-                ?.GetValue(null);
-
-            var rogueId = rogueSkill?.GetType().GetProperty("Id")?.GetValue(rogueSkill);
-            var paladinId = paladinSkill?.GetType().GetProperty("Id")?.GetValue(paladinSkill);
-
-            Monitor.Log($"RogueSkill ID={rogueId}", LogLevel.Info);
-            Monitor.Log($"PaladinSkill ID={paladinId}", LogLevel.Info);
-
             var getLevel = AccessTools.Method(
                 AccessTools.TypeByName("SpaceCore.SkillExtensions"),
                 "GetCustomSkillLevel",
                 new[] { typeof(Farmer), typeof(string) });
+            var getExp = AccessTools.Method(
+                AccessTools.TypeByName("SpaceCore.Skills"),
+                "GetExperienceFor",
+                new[] { typeof(Farmer), typeof(string) });
 
-            int rogueLevel = (int)(getLevel?.Invoke(null, new object[] { Game1.player, rogueId?.ToString() ?? "" }) ?? 0);
-            int paladinLevel = (int)(getLevel?.Invoke(null, new object[] { Game1.player, paladinId?.ToString() ?? "" }) ?? 0);
+            int rogueLevel = (int)(getLevel?.Invoke(null, new object[] { Game1.player, "DestyNova.SwordAndSorcery.Rogue" }) ?? 0);
+            int rogueXP = (int)(getExp?.Invoke(null, new object[] { Game1.player, "DestyNova.SwordAndSorcery.Rogue" }) ?? 0);
+            int paladinLevel = (int)(getLevel?.Invoke(null, new object[] { Game1.player, "DestyNova.SwordAndSorcery.Paladin" }) ?? 0);
+            int paladinXP = (int)(getExp?.Invoke(null, new object[] { Game1.player, "DestyNova.SwordAndSorcery.Paladin" }) ?? 0);
 
             int expectedBonus = rogueLevel * 3 + paladinLevel * 5;
             int expectedMaxHealth = 100 + expectedBonus;
 
-            Monitor.Log($"SaveLoaded: Rogue level={rogueLevel}, Paladin level={paladinLevel}", LogLevel.Info);
+            Monitor.Log($"SaveLoaded: Rogue level={rogueLevel}, XP={rogueXP}", LogLevel.Info);
+            Monitor.Log($"SaveLoaded: Paladin level={paladinLevel}, XP={paladinXP}", LogLevel.Info);
             Monitor.Log($"SaveLoaded: expectedBonus={expectedBonus}, currentMaxHealth={Game1.player.maxHealth}, expectedMaxHealth={expectedMaxHealth}", LogLevel.Info);
             Monitor.Log($"SaveLoaded: bonus applied correctly={Game1.player.maxHealth >= expectedMaxHealth}", LogLevel.Info);
         };
