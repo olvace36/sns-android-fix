44529f0360a778e2af127f445d475c70e4452614
olvace36 <120215414+olvace36@users.noreply.github.com>
2026-06-15 09:06:25 +0700

    Update RevalidateHealthPatch.cs

--- RevalidateHealthPatch.cs
+++ RevalidateHealthPatch.cs
@@ -13,6 +13,31 @@ public class RevalidateHealthPatch
 {
     internal static IMonitor? Monitor;
 
+    // cache reflection
+    private static MethodInfo? _getBuffed;
+    private static MethodInfo? _getBase;
+    private static object? _rogueSkill;
+    private static object? _paladinSkill;
+
+    public static void InitCache()
+    {
+        var skillType = AccessTools.TypeByName("SpaceCore.Skills+Skill");
+        _getBuffed = AccessTools.Method(
+            AccessTools.TypeByName("SpaceCore.SkillExtensions"),
+            "GetCustomBuffedSkillLevel",
+            new[] { typeof(Farmer), skillType });
+        _getBase = AccessTools.Method(
+            AccessTools.TypeByName("SpaceCore.SkillExtensions"),
+            "GetCustomSkillLevel",
+            new[] { typeof(Farmer), skillType });
+        _rogueSkill = AccessTools.TypeByName("SwordAndSorcerySMAPI.ModSnS")
+            ?.GetProperty("RogueSkill", BindingFlags.Public | BindingFlags.Static)
+            ?.GetValue(null);
+        _paladinSkill = AccessTools.TypeByName("SwordAndSorcerySMAPI.ModTOP")
+            ?.GetProperty("PaladinSkill", BindingFlags.Public | BindingFlags.Static)
+            ?.GetValue(null);
+    }
+
     static int CalcVanillaBaseMaxHealth(Farmer farmer)
     {
         int num = 100;
@@ -30,42 +55,18 @@ public class RevalidateHealthPatch
         return num;
     }
 
-    static void GetSkillLevels(Farmer farmer,
-        out int rogueBase, out int rogueBuffed,
-        out int paladinBase, out int paladinBuffed)
-    {
-        var skillType = AccessTools.TypeByName("SpaceCore.Skills+Skill");
-        var getBuffed = AccessTools.Method(
-            AccessTools.TypeByName("SpaceCore.SkillExtensions"),
-            "GetCustomBuffedSkillLevel",
-            new[] { typeof(Farmer), skillType });
-        var getBase = AccessTools.Method(
-            AccessTools.TypeByName("SpaceCore.SkillExtensions"),
-            "GetCustomSkillLevel",
-            new[] { typeof(Farmer), skillType });
-
-        var rogueSkill = AccessTools.TypeByName("SwordAndSorcerySMAPI.ModSnS")
-            ?.GetProperty("RogueSkill", BindingFlags.Public | BindingFlags.Static)
-            ?.GetValue(null);
-        var paladinSkill = AccessTools.TypeByName("SwordAndSorcerySMAPI.ModTOP")
-            ?.GetProperty("PaladinSkill", BindingFlags.Public | BindingFlags.Static)
-            ?.GetValue(null);
-
-        rogueBase = rogueSkill != null
-            ? (int)(getBase?.Invoke(null, new object[] { farmer, rogueSkill }) ?? 0) : 0;
-        rogueBuffed = rogueSkill != null
-            ? (int)(getBuffed?.Invoke(null, new object[] { farmer, rogueSkill }) ?? 0) : 0;
-        paladinBase = paladinSkill != null
-            ? (int)(getBase?.Invoke(null, new object[] { farmer, paladinSkill }) ?? 0) : 0;
-        paladinBuffed = paladinSkill != null
-            ? (int)(getBuffed?.Invoke(null, new object[] { farmer, paladinSkill }) ?? 0) : 0;
-    }
-
     static void Postfix(Farmer farmer)
     {
-        GetSkillLevels(farmer,
-            out int rogueBase, out int rogueBuffed,
-            out int paladinBase, out int paladinBuffed);
+        if (_getBuffed == null || _getBase == null) return;
+
+        int rogueBase = _rogueSkill != null
+            ? (int)(_getBase.Invoke(null, new object[] { farmer, _rogueSkill }) ?? 0) : 0;
+        int rogueBuffed = _rogueSkill != null
+            ? (int)(_getBuffed.Invoke(null, new object[] { farmer, _rogueSkill }) ?? 0) : 0;
+        int paladinBase = _paladinSkill != null
+            ? (int)(_getBase.Invoke(null, new object[] { farmer, _paladinSkill }) ?? 0) : 0;
+        int paladinBuffed = _paladinSkill != null
+            ? (int)(_getBuffed.Invoke(null, new object[] { farmer, _paladinSkill }) ?? 0) : 0;
 
         int vanillaBase = CalcVanillaBaseMaxHealth(farmer);
         int snsBaseBonus = rogueBase * 3 + paladinBase * 5;
